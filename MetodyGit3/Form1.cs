c96eb3ab675a47ff244fa823355884d985a12fcc
david malena <davidmalena@seznam.cz>
2021-03-21 18:33:42 +0100

    Přidejte soubory projektu.

---
+++ MetodyGit3/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetodyGit3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime dt1 = DateTime.Parse(textBox1.Text);
+            DateTime dt2 = DateTime.Parse(textBox2.Text);
+
+            TimeSpan casovyInterval = dt2 - dt1;
+            MessageBox.Show("Počet dní, které uplynuly mezi těmito daty: " + casovyInterval.TotalDays);
+        }
+    }
+}
